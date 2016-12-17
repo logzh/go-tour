ea91bd2c95f6357a61b4cc01336001f10be2b31f
logzh <zjhcug@qq.com>
2016-12-17 23:40:59 +0800

    update

--- src/basics/method.go
+++ src/basics/method.go
@@ -22,6 +22,45 @@ func (c Circle)area()(float64)  {
 	return c.radius * c.radius*math.Pi
 }
 
+const(
+	WHITE = iota
+	BLACK
+	BLUE
+	RED
+	YELLOW
+)
+
+type Color byte
+
+type Box struct {
+	width, height, depth float64
+	color Color
+}
+
+type BoxList []Box
+
+func (b Box)volume()(float64)  {
+	return b.depth * b.height * b.width
+}
+
+func (b *Box)setColor(c Color)  {
+	b.color = c
+}
+
+func (bl BoxList)getBiggestBoxColor()(Color)  {
+	v := 0.0
+	k := Color(WHITE)
+
+	for _, b := range bl{
+		if vol := b.volume(); vol > v{
+			v = vol
+			k = b.color
+		}
+	}
+
+	return k
+}
+
 func init()  {
 	fmt.Println("method的用法：")
 
@@ -30,4 +69,12 @@ func init()  {
 
 	c := Circle{8.9}
 	fmt.Println("圆的面积：", c.area())
+
+	boxes := BoxList{Box{1, 2, 3, WHITE}, Box{1, 9, 3, YELLOW}, Box{1.8, 2.9, 3.9, BLACK}}
+
+	boxes[len(boxes)-1].setColor(RED)
+
+	fmt.Println("boxes length:", len(boxes))
+	fmt.Println("get a box color:", boxes[len(boxes)-1].color)
+	fmt.Println("the max box color:", boxes.getBiggestBoxColor())
 }
